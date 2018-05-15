f2dcee2971a2332e76d5e5308769685ec153e34a
sscanni <steven.scanniello@snhu.edu>
2018-05-14 23:15:03 -0400

    another change

--- Udemy/2_6/server.js
+++ Udemy/2_6/server.js
@@ -2,7 +2,10 @@ var http = require('http');
 
 var handleRequest = function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("Welcome to Node Training and Fundamentals\n");
+    var m1 = "Welcome to Node Training and Fundamentals\n" 
+    var m2 = "This is another line\n" 
+    //res.end("Welcome to Node Training and Fundamentals\n");
+    res.end(m1 + m2);
 };
 
 var server = http.createServer(handleRequest);
