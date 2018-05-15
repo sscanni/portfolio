c6c121453e2960b4147fc1a76082ff5a48b99632
sscanni <steven.scanniello@snhu.edu>
2018-05-14 22:35:14 -0400

    Changes made 2018-05-14 10:35PM

--- Udemy/2_5/example.js
+++ Udemy/2_5/example.js
@@ -1,2 +1,4 @@
 var announce = require('./announce');
-announce('Node Training and Fundamentals');
+//var m = 'Node Training and Fundamentals';
+var m = 'This is a message to display';
+announce(m);
