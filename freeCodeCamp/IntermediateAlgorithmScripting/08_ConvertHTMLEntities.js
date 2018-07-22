b8f7008963e571ee5dd1a1badfcc72536379c4f4
sscanni <steven.scanniello@snhu.edu>
2018-07-22 11:08:10 -0400

    commit 2018-07-22

--- freeCodeCamp/IntermediateAlgorithmScripting/08_ConvertHTMLEntities.js
+++ freeCodeCamp/IntermediateAlgorithmScripting/08_ConvertHTMLEntities.js
@@ -1,9 +1,15 @@
 function convertHTML(str) {
     // &colon;&rpar;
+
+    str = str.replace(/&/g, '&amp;');
+    str = str.replace(/</g, '&lt;');
+    str = str.replace(/>/g, '&gt;');
+    str = str.replace(/"/g, '&quot;');
+    str = str.replace(/'/g, '&apos;');
     return str;
   }
   
-  convertHTML("Dolce & Gabbana");
+  console.log(convertHTML("Dolce & Gabbana's"));
 /*
 
 Intermediate Algorithm Scripting: Convert HTML Entities
